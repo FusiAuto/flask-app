70b6db7c2cce85ff8c0db759727213d038817c83
b-bots-dev <b.bots.dev@gmail.com>
2023-09-11 18:27:09 +0200

    1 commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, send_file, url_for
+
+app = Flask('recorder')
+
+
+@app.route('/')
+def home():
+    path = 'static/render - start command.png'
+    # return send_file(path)
+    url = url_for('static', filename='render - start command.png')
+    return url
+
+
+app.run()
+# http://127.0.0.1:5000/static/render%20-%20start%20command.png
